e16865fbcbc3f816d748e478bbefb8feefc0af83
Jack Vento <jackovento@gmail.com>
2021-10-04 21:04:29 -0700

    Update nm_pathfinder.py

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -22,14 +22,65 @@ def find_path (source_point, destination_point, mesh):
     for box in mesh["boxes"]:
         if contains_point(box, source_point):
             boxes['start'] = box
-            #print(f'debug: start - {box}')
         if contains_point(box, destination_point):
             boxes['goal'] = box
-            #print(f'debug: goal - {box}')
 
     if 'start' not in boxes or 'goal' not in boxes:
         print('No path!')
         return path, {}
+
+    def astar(start, goal, graph, adj):
+        paths = {start: []}
+        pathcosts = {start: 0}
+
+        queue = []
+        heappush(queue, (0, start))  # maintain a priority queue of cells
+
+        detail_points = {start: source_point, goal: destination_point}
+        whole_points = {start: source_point, goal: destination_point}
+
+        line_path = []
+
+        while queue:
+            priority, cell = heappop(queue)
+            boxes[cell] = cell
+            if cell == goal:
+                line_path.append(destination_point)
+                while cell != start:
+
+                    dx = detail_points[cell][0]
+                    dy = detail_points[cell][1]
+                    cell = paths[cell]
+
+                    if dx <= cell[0]: dx = cell[0]
+                    if dx >= cell[1]: dx = cell[1]
+                    if dy <= cell[2]: dy = cell[2]
+                    if dy >= cell[3]: dy = cell[3]
+
+                    detail_points[cell] = (dx, dy)
+                    line_path.insert(0, detail_points[cell])
+                line_path.insert(0, source_point)
+                return line_path
+
+            # investigate children
+            for child in adj[cell]:
+                dx = whole_points[cell][0]
+                dy = whole_points[cell][1]
+
+                if dx <= child[0]: dx = child[0]
+                if dx >= child[1]: dx = child[1]
+                if dy <= child[2]: dy = child[2]
+                if dy >= child[3]: dy = child[3]
+                whole_points[child] = (dx, dy)
+
+                # calculate cost along this path to child
+                cost_to_child = priority + transition_cost(whole_points[cell], whole_points[child], destination_point)
+                if child not in pathcosts or cost_to_child < pathcosts[child]:
+                    pathcosts[child] = cost_to_child  # update the cost
+                    paths[child] = cell  # set the backpointer
+                    heappush(queue, (cost_to_child, child))  # put the child on the priority queue
+
+        return []
 
     def bidirectional_astar(start, goal, graph, adj):
         forward_prev_paths = {start: None}
@@ -63,12 +114,11 @@ def find_path (source_point, destination_point, mesh):
                     backward_curr = backward_prev_paths[backward_curr]
                     line_path = path_forward + path_backward
                 return line_path
-            #if (curr_goal == 'destination' and cell in backward_prev_paths) \
+            # if (curr_goal == 'destination' and cell in backward_prev_paths) \
             #        or (curr_goal == 'start' and cell in forward_prev_paths):
             #     line_path.append(destination_point)
             #     while cell != start:
-            #         dx = detail_points[cell][0]
-            #         dy = detail_points[cell][1]
+            #         dx, dy = detail_points[cell]
             #
             #         if curr_goal == 'destination':
             #             cell = forward_prev_paths[cell]
@@ -85,6 +135,7 @@ def find_path (source_point, destination_point, mesh):
             #         #print(line_path)
             #     line_path.insert(0, source_point)
             #     return line_path
+
             # investigate children
             for child in adj[cell]:
                 if curr_goal == 'destination':
@@ -115,56 +166,15 @@ def find_path (source_point, destination_point, mesh):
         print('No path!')
         return []
 
-    def transition_cost(cell, cell2, final_cell):
-        distance = euclidean_distance(cell, cell2)
-        estimated_distance = euclidean_distance(cell, final_cell)
-        #print(f'debug: distance: {distance}')
-        #print(f'debug: estimated_distance: {estimated_distance}')
-        return distance + estimated_distance
-
-    def breadth_first_search (start, goal, graph, adj):
-        queue = [start]
-        prevs = {start: None}
-        detail_points = {start: source_point, goal: destination_point}
-        line_path = []
-        while queue:
-            current_node = queue.pop(0)
-            boxes[current_node] = current_node
-            if current_node == goal:
-                line_path.append(destination_point)
-                detail_points[goal] = destination_point
-                print('Path!')
-                while current_node != start:
-                #    returnPath.append((current_node[0], current_node[1], current_node[2], current_node[3]))
-
-                    dx = detail_points[current_node][0]
-                    dy = detail_points[current_node][1]
-                    current_node = prevs[current_node]
-
-                    if dx <= current_node[0]: dx = current_node[0]
-                    if dx >= current_node[1]: dx = current_node[1]
-                    if dy <= current_node[2]: dy = current_node[2]
-                    if dy >= current_node[3]: dy = current_node[3]
-
-                    detail_points[current_node] = (dx, dy)
-                    line_path.insert(0, detail_points[current_node])
-                    print(f'debug: inserted {detail_points[current_node]}')
-                    print(line_path)
-                line_path.insert(0, source_point)
-                return line_path
-            else:
-                for new in adj[current_node]:
-                    if new not in prevs:
-                        prevs[new] = current_node
-                        queue.append(new)
-
-        print('No path!')
-        return line_path
-
-    path = bidirectional_astar(boxes['start'], boxes['goal'], mesh["boxes"], mesh["adj"])
-    #path = breadth_first_search(boxes['start'], boxes['goal'], mesh["boxes"], mesh["adj"])
+    # path = bidirectional_astar(boxes['start'], boxes['goal'], mesh["boxes"], mesh["adj"])
+    path = astar(boxes['start'], boxes['goal'], mesh["boxes"], mesh["adj"])
 
     return path, boxes.values()
+
+def transition_cost(cell, cell2, final_cell):
+    distance = euclidean_distance(cell, cell2)
+    estimated_distance = euclidean_distance(cell, final_cell)
+    return distance + estimated_distance
 
 def contains_point(box, point):
     # bx1 <= p.x && p.x <= bx2 && by1 <= p.y && p.y <= bx1
